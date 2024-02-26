f2efffecfcfdef8c0f0528bed737d9277b109aa4
davide-piarulli <davide.piarulli.1@gmail.com>
2024-02-26 15:37:48 +0000

    milestone 3

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,19 @@
 /*
 1. inserisco tutte le img nell'array
-2. Creo un ciclo FOR ed inserisco nel items-wrapper gli elementi HTML che contengono le img con già classe hide
+2. Creo un ciclo FOR ed inserisco nel items-wrapper gli elementi HTML che contengono le img con già classe hide. inizializzo un contatore counterImg a 0.
   2.a mostro solo la prima img
-3. 
+3. dichiaro le variabili const per le frecce.
+Creo un addEventListener che al loro click deve nascondere img attuale e incrementare o decrementare il counterImg
+  3.a dopo aver incrementato/decrementato il counterImg, mostro img attuale
+
 */
 
 
 // Elements
 const itemsWrapper = document.querySelector('.items-wrapper');
 const itemsCollection = document.getElementsByClassName('img');
+const up = document.querySelector('.up');
+const down = document.querySelector('.down');
 
 let counterImg = 0;
 
@@ -27,10 +32,26 @@ console.log(images);
 for(let i = 0; i < images.length; i++){
   const img = images[i];
   itemsWrapper.innerHTML += `<img class="img hide" src="${img}">`
-  
 }
 
 // 2.a
 itemsCollection[counterImg].classList.remove('hide');
 
+// 3
+up.addEventListener('click', function(){
+  itemsCollection[counterImg].classList.add('hide');
+  counterImg--;
 
+  // 3.a
+  itemsCollection[counterImg].classList.remove('hide');
+})
+
+
+// 3
+down.addEventListener('click', function(){
+  itemsCollection[counterImg].classList.add('hide');
+  counterImg++;
+  
+  // 3.a
+  itemsCollection[counterImg].classList.remove('hide');
+})
